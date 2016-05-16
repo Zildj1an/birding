4c5439bc373c79eea0d66183d2e29d6d85daa27e
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-05-17 00:34:27 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+   main.py
+
+   Descp: Main for the app
+
+   Created on: 17-may-2016
+
+   Copyright 2016 Akronix <akronix5@gmail.com>
+"""
+
+
+
+def main():
+
+  return 0
+
+if __name__ == '__main__':
+  main()
+
